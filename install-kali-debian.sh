1cd18a28e2a18d97c84f6f831555ab242c9032e4
Jean-Nicolas Boulay <jn@yaloub.com>
2022-05-15 15:10:17 -0400

    Added instructions for installing Kali pck source. Also added to the script install file.

--- install-kali-debian.sh
+++ install-kali-debian.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cp /etc/apt/sources.list /etc/apt/sources.list.bck
-echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
-apt update
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bck
+sudo echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
+gpg --keyserver pgpkeys.mit.edu --recv-key  ED444FF07D8D0BF6
+sudo gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
+sudo apt update
