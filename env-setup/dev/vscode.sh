b0438c83f1b2ba06f8a5df2e9486d6c19f7d23f5
jnbdz <237008+jnbdz@users.noreply.github.com>
2022-07-11 14:33:19 -0400

    Added some scripts to install applications for different env.

---
+++ env-setup/dev/vscode.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+declare vsCodeVersion="1.69.0-1657183742"
+
+installVSCode() {
+    wget -c https://az764295.vo.msecnd.net/stable/92d25e35d9bf1a6b16f7d0758f25d48ace11e5b9/code_${vsCodeVersion}_amd64.deb -P ~/Downloads/
+    dpkg -i ~/Downloads/code_${vsCodeVersion}_amd64.deb
+}
+
+removeVSCodedeb() {
+    rm ~/Downloads/code_${vsCodeVersion}_amd64.deb
+}
+
+uninstallVSCode() {
+    
+}
