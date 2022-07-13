0f66f907bcf6dda161b159930a24bf8c6e419514
Jean-Nicolas Boulay <jn@yaloub.com>
2022-07-13 12:23:53 -0400

    Removed old install scripts and put all the install scripts in the Makefile

--- envs/basic/monitoring.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-installBasic() {
-    sudo apt-get install -y htop
-}
-
-uninstallBasic() {
-    sudo apt purge -y htop
-}
--- envs/basic/tools.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-installBasic() {
-    sudo apt-get install -y vim curl
-}
-
-uninstallBasic() {
-    sudo apt purge -y vim curl
-}
