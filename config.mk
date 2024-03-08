1a8ed39d50dddff503e7c07fc8ae7a3f0a761600
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-03-08 18:44:36 -0500

    Clean up. Added more stuff to install

--- config.mk
+++ config.mk
@@ -8,6 +8,7 @@ DWM_GITHUB_USER = $(GITHUB_USER)
 DWM_VERSION = 6.4
 
 # Dev IDEs versions
+soapuiVersion = 5.7.2
 androidStudioVersion = 2021.2.1.15
 ideaVersion = 2023.3.4
 pycharmVersion = 2022.1.3
